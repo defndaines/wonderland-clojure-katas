07736713f1a0f0e155f98b7d1cdd1de699fed41e
defndaines <defndaines@gmail.com>
2015-09-28 21:46:08 -0700

    In progress work on doublets kata.
    Rough structure in place, but recursion isn't quite right. It overflows
    when testing at the REPL.

--- doublets/src/doublets/solver.clj
+++ doublets/src/doublets/solver.clj
@@ -2,10 +2,35 @@
   (:require [clojure.java.io :as io]
             [clojure.edn :as edn]))
 
+;; For REPL
+(require '[clojure.java.io :as io])
+(require '[clojure.edn :as edn])
+;; End For REPL
+
 (def words (-> "words.edn"
                (io/resource)
                (slurp)
                (read-string)))
+(defn off-by-one [word1 word2]
+  (let [base (vec word1)]
+    (= 1 (count (filter (comp not zero?)
+                        (map compare base (vec word2)))))))
+
+;; (check "head" "heal" words [])
+
+(defn check [word safe-word dict acc]
+  (if (= word safe-word)
+    acc
+    (if-let [candidates (filter #(off-by-one word %) dict)]
+      (let [so-far (set acc)
+            valid (filter so-far candidates)]
+        (if (seq valid)
+          (recur (first valid) safe-word dict (conj acc (first valid))))))))
+
+;; (doublets "head" "teal")
 
 (defn doublets [word1 word2]
-  "make me work")
+  (if (= (count word1) (count word2))
+    (let [dict (filter #(= (count word1) (count %)) words)]
+      (check word1 word2 dict []))
+    []))
